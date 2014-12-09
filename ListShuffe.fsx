bf7a17bcabedf54625ed53fa3c4e1ae127b7ed7e
Antonio Prestes García <antonio.garcia2@gmail.com>
2014-12-09 17:35:08 +0100

    Update ListShuffe.fsx

--- ListShuffe.fsx
+++ ListShuffe.fsx
@@ -1,5 +1,19 @@
 open System
 
+let drop i l =
+    match l  with
+    | [] -> []
+    | h :: t -> drop' i l 0 []
+    
+let rec drop' i l ii ll = 
+    match l with
+    | [] -> []  
+    | h :: t -> if List.length ll = List.length l - 1 then ll
+                else
+                   let iii = if ii = i-1 then ii+2 else ii+1 
+                   let ll = List.append ll [l.Item iii-1]
+                   drop' i l iii ll 
+     
 // -- | Pop one element from a specific list position
 let pop i l =
     1
@@ -18,5 +32,7 @@ let main() =
     let v = [1..10]
     Console.WriteLine("List Shuffle!")
     Console.WriteLine(shuffle v)
+    Console.WriteLine(drop 1 v)
+    Console.WriteLine(List.length v)
     
 main()
