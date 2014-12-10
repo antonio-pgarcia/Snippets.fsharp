8c56d6b1e68f2c1ebbc40491e115761fbefd3499
Antonio Prestes García <antonio.garcia2@gmail.com>
2014-12-10 12:47:34 +0100

    Update ListShuffe.fsx

--- ListShuffe.fsx
+++ ListShuffe.fsx
@@ -1,38 +1,46 @@
 open System
+  
+// -- | Auxiliary recursive drop function
+let rec drop' i l1 p l2 =
+    if List.length l2 = List.length l1 - 1 then l2
+    else 
+        let p = if p = i then p+1  else p 
+        drop' i l1 (p+1) (List.append l2 [(List.nth l1 p)])
 
+// -- | Removes the the nth element from list
 let drop i l =
-    match l  with
-    | [] -> []
-    | h :: t -> drop' i l 0 []
-    
-let rec drop' i l ii ll = 
-    match l with
-    | [] -> []  
-    | h :: t -> if List.length ll = List.length l - 1 then ll
-                else
-                   let iii = if ii = i-1 then ii+2 else ii+1 
-                   let ll = List.append ll [l.Item iii-1]
-                   drop' i l iii ll 
-     
+    drop' i l 0 []        
+
 // -- | Pop one element from a specific list position
 let pop i l =
-    1
+    match l with
+    | [] -> ([],[])
+    | h :: t when i >= 0 -> 
+        let e = [l.Item i]
+        let ll = drop i l
+        (e,ll)
+    | _ -> ([],[])
 
 // -- | Generate random number within interval a-b with seed s
 let myrandom a b s =
     let r = System.Random(s)
     r.Next(a, b)
 
+// -- | Shuffle' auxiliary
+let rec shuffle' r l1 l2 =
+    let len = List.length l1
+    let t = pop r l1
+    if len = 0 then l2
+    else shuffle' (myrandom 0 (len-1) r) (snd t) (List.append l2 (fst t))  
+
 // -- | Shuffle a list
 let shuffle l =
-    l
+    shuffle' (myrandom 0 ((List.length l)-1) 0) l []
    
 // -- | The main entry point.
 let main() = 
-    let v = [1..10]
+    let v = [1..100]
     Console.WriteLine("List Shuffle!")
-    Console.WriteLine(shuffle v)
-    Console.WriteLine(drop 1 v)
-    Console.WriteLine(List.length v)
+    printfn "%A" (shuffle v)
     
 main()
